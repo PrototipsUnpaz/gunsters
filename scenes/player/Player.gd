d7de298a1505034da1c8ec069f8eac962b2778b6
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-04-18 11:59:47 -0300

    Primer_paso_mov

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,19 +1,7 @@
 extends KinematicBody2D
 
-const SPEED = 300
-var velocity = Vector2()
-
-func _physics_process(delta):
-	if Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
-		velocity.x = 0
-		velocity.y = 0
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -SPEED
-	if Input.is_action_pressed("ui_down"):
-		velocity.y = SPEED
-	move_and_slide(velocity)
-	pass
+func _process(delta):
+	 look_at_mouse()
+		
+func look_at_mouse():
+	look_at(get_global_mouse_position())
