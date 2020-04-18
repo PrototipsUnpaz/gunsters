ec258614c1ab25bb32317ed231adc4191fc13f34
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-04-18 12:44:59 -0300

    Primer_paso_mov2

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,7 +1,18 @@
 extends KinematicBody2D
-
+var Velocity = Vector2 (0,0)
 func _process(delta):
 	 look_at_mouse()
 		
 func look_at_mouse():
-	look_at(get_global_mouse_position())
+	if get_global_mouse_position().x > 300:
+		Velocity.x = 0
+		$Sprite.flip_h = true
+	#if get_global_mouse_position().y > 300:
+		#Velocity.y = 200
+	if get_global_mouse_position().x <= 300:
+		Velocity.x = 0
+		$Sprite.flip_h = false
+	#if get_global_mouse_position().y <= 300:
+		#Velocity.y =-200
+	move_and_slide(Velocity)
+	pass
