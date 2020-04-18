0c141a653644824fef95d47dc0ad783b64ca23a5
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-04-18 13:22:15 -0300

    Primer_paso_mov4

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -4,14 +4,21 @@ func _process(delta):
 	 look_at_mouse()
 		
 func look_at_mouse():
-	if get_global_mouse_position().x > 300:
+	if get_global_mouse_position().x > 300 and get_global_mouse_position().y < 500:
 		Velocity.x = 0
 		$Pistolani.flip_h = true
 		$Pistolani.frame = 0
-	if get_global_mouse_position().x <= 300:
+	if get_global_mouse_position().x <= 300 and get_global_mouse_position().y < 500:
 		Velocity.x = 0
 		$Pistolani.flip_h = false
 		$Pistolani.frame = 0
-	
+	if get_global_mouse_position().x <= 300 and get_global_mouse_position().y > 500:
+		Velocity.x = 0
+		$Pistolani.flip_h = true
+		$Pistolani.frame = 2
+	if get_global_mouse_position().x > 300 and get_global_mouse_position().y > 500:
+		Velocity.x = 0
+		$Pistolani.flip_h = false
+		$Pistolani.frame = 2
 	move_and_slide(Velocity)
 	pass
