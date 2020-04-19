905080c346e3498c9feb736fdfb4689e15f4b68b
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-04-19 16:36:28 -0300

    Primer_paso_mov5

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,24 +1,27 @@
 extends KinematicBody2D
 var Velocity = Vector2 (0,0)
 func _process(delta):
-	 look_at_mouse()
-		
+	look_at_mouse()
 func look_at_mouse():
-	if get_global_mouse_position().x > 300 and get_global_mouse_position().y < 500:
+	if get_global_mouse_position().x > 700 and get_global_mouse_position().y < 400:
 		Velocity.x = 0
 		$Pistolani.flip_h = true
 		$Pistolani.frame = 0
-	if get_global_mouse_position().x <= 300 and get_global_mouse_position().y < 500:
+	if get_global_mouse_position().x <= 700 and get_global_mouse_position().y < 400:
 		Velocity.x = 0
 		$Pistolani.flip_h = false
 		$Pistolani.frame = 0
-	if get_global_mouse_position().x <= 300 and get_global_mouse_position().y > 500:
+	if Input.is_action_pressed("Pistolani"):
+		$Pistolani.frame = 1
+	if get_global_mouse_position().x <= 700 and get_global_mouse_position().y > 400:
 		Velocity.x = 0
 		$Pistolani.flip_h = true
 		$Pistolani.frame = 2
-	if get_global_mouse_position().x > 300 and get_global_mouse_position().y > 500:
+	if get_global_mouse_position().x > 700 and get_global_mouse_position().y > 400:
 		Velocity.x = 0
 		$Pistolani.flip_h = false
 		$Pistolani.frame = 2
-	move_and_slide(Velocity)
+	if  Input.is_action_pressed("Pistolani") and get_global_mouse_position().y > 400: 
+		$Pistolani.frame = 3
+		move_and_slide(Velocity)
 	pass
