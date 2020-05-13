9f855f3fef3a2aa16a00ef815f5475d520625d20
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-05-12 21:42:01 -0300

    Enemy_2

--- scenes/Enemy_2.gd
+++ scenes/Enemy_2.gd
@@ -2,7 +2,9 @@ extends Area2D
 
 
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
+
+
+func _on_Enemy_2_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			var mainnodes = load ("res://scenes/worldtest.gd").new()
--- scenes/WorldTest.gd
+++ scenes/WorldTest.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 var Enemy = preload("res://scenes/Enemy.tscn/")
-
+var Enemy_2 = preload ("res://scenes/Enemy_2.tscn/")
 func _ready():
 	add_new_scenes(300,400)
-	add_new_scenes(300,400)
+	add_new_scenes_2(300,500)
 	pass
 	
 func add_new_scenes(mtop,mleft):
@@ -12,6 +12,12 @@ func add_new_scenes(mtop,mleft):
 	add_child(mobject)
 	mobject.set_position(Vector2(mtop,mleft))
 	pass
-
+	
+func add_new_scenes_2(mtop,mleft):
+	var mobject2 = Enemy_2.instance()
+	add_child(mobject2)
+	mobject2.set_position(Vector2(mtop,mleft))
+	pass
+	
 func remove_objects(mainnode,objectpath):
 	mainnode.get_node(objectpath).queue_free()
