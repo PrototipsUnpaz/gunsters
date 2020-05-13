0833d26fba52f49f82f7e216a2ab6087752e81b1
Leonardo Maidana <leo-lomas1@hotmail.com>
2020-05-13 17:23:02 -0300

    Enemy_2 timer_conect

--- scenes/Enemy_2.gd
+++ scenes/Enemy_2.gd
@@ -2,11 +2,14 @@ extends Area2D
 
 
 
-
-
 func _on_Enemy_2_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			var mainnodes = load ("res://scenes/worldtest.gd").new()
 			mainnodes.remove_objects(get_parent(), get_path())
 	pass 
+
+
+func _on_Timer_timeout():
+	
+	pass
