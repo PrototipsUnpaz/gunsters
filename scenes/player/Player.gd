b7b91e15d0ff1d9506fe7f15872b1ec069c590d1
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-05-21 18:41:30 -0300

    Animación_Arma2

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -7,9 +7,26 @@ func _ready():
 	pass
 func _process(delta):
 	$Pistolani.frame = PosPistol
-	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+	if Input.is_action_just_released("Touch") and CountCharge >= 0:
 		Get_Direction()
-		
+	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+		if get_global_mouse_position().x > 466 and get_global_mouse_position().y < 500:
+			PosPistol = 1
+	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+		if get_global_mouse_position().x < 233 and get_global_mouse_position().y < 500:
+			$Pistolani.flip_h = false
+			PosPistol = 1
+	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+		if get_global_mouse_position().x < 233 and get_global_mouse_position().y > 500:
+			$Pistolani.flip_h = false
+			PosPistol = 3
+	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+		if get_global_mouse_position().x > 466 and get_global_mouse_position().y > 500:
+			$Pistolani.flip_h = true
+			PosPistol = 3
+	if Input.is_action_just_pressed("Touch") and CountCharge >= 0:
+		if get_global_mouse_position().x > 234 and get_global_mouse_position().x < 465:
+			PosPistol = 5
 	pass
 	
 func Get_Direction():
