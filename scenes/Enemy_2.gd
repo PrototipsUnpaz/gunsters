f044f40355d043c46524bdd62087e9c680210afd
Leonardo Maidana <leo-lomas1@hotmail.com>
2020-05-26 14:44:05 -0300

    Timers_enemy_2
    Se agrego timers y scripts del enemigo 2

--- scenes/Enemy_2.gd
+++ scenes/Enemy_2.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-
-
 func _on_Enemy_2_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
@@ -9,7 +7,6 @@ func _on_Enemy_2_input_event(viewport, event, shape_idx):
 			mainnodes.remove_objects(get_parent(), get_path())
 	pass 
 
-
 func _on_Timer_timeout():
-	5
+	get_tree().call_group("Flash_enemy", "mostrar_Flash")
 	pass
