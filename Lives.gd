62899b08cc826234d4c99d55890a211e8d935b48
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-05-28 11:48:07 -0300

    Arreglos_Improvisto

--- Lives.gd
+++ Lives.gd
@@ -3,4 +3,7 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-
+func _process(delta):
+	Autoload.Lives = 1
+	$Anilivescol.frame = 1
+	pass
--- scenes/Enemy_2.gd
+++ scenes/Enemy_2.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 func _on_Enemy_2_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and Autoload.Charge >= 1:
-		if event.is_pressed():
+	if event is InputEventScreenTouch and Autoload.Charge >= 1:
+		if event.is_touched():
 			var mainnodes = load ("res://scenes/worldtest.gd").new()
 			mainnodes.remove_objects(get_parent(), get_path())
 	pass 
@@ -10,4 +10,6 @@ func _on_Enemy_2_input_event(viewport, event, shape_idx):
 func _on_Timer_timeout():
 	get_tree().call_group("Flash_enemy", "mostrar_Flash")
 	queue_free()
+	Autoload.Lives -= 1
+	print (Autoload.Lives)
 	pass
