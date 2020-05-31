8a9db4dcf02270faa0823841452f11bc38839033
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-05-31 13:13:04 -0300

    Mov_3

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -7,7 +7,7 @@ func _ready():
 	pass
 	
 func interpolate_position():
-	$EnemyTween.interpolate_property(self, "position", self.position + Vector2(400,500), self.position + Vector2(800,500), 5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$EnemyTween.interpolate_property(self, "position", self.position + Vector2(400,500),self.position + Vector2(800,500), 5, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$EnemyTween.start()
 	yield($EnemyTween,"tween_completed")
 	queue_free()
---
+++ scenes/Respawn_3.gd
@@ -0,0 +1,13 @@
+extends Position2D
+
+export (PackedScene) var ene2
+func _ready():
+	pass
+
+
+
+func _on_Respawn2_timeout():
+	var NewEnemy2 = ene2.instance()
+	get_tree().get_nodes_in_group("WorldTest")[0].add_child(NewEnemy2)
+	NewEnemy2.global_position = global_position
+	pass 
