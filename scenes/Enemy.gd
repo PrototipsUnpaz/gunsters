93b9b219c3628a607e02f2dacba8e33a7e3ee27f
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-05-31 14:13:34 -0300

    Resuelto

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,22 +1,14 @@
 extends KinematicBody2D
-var enemyInitialPosition
 
+var speed = 50
 func _ready():
-	enemyInitialPosition = true
-	interpolate_position()
+	$DeleteThis.start()
 	pass
-	
-func interpolate_position():
-	$EnemyTween.interpolate_property(self, "position", self.position + Vector2(400,500),self.position + Vector2(800,500), 5, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	$EnemyTween.start()
-	yield($EnemyTween,"tween_completed")
+
+func _physics_process(delta):
+	move_local_x(speed * delta)
+
+
+func _on_DeleteThis_timeout():
 	queue_free()
-	pass
-
-func _on_EnemyTween_tween_completed(object, key):
-	enemyInitialPosition = !enemyInitialPosition
-	interpolate_position()
-	pass
-
-
-
+	pass 
