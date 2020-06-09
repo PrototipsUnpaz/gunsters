c76b5d0078afcb70abc3b67eda0588b322b4b173
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-06-09 00:55:48 -0300

    Sound2

--- scenes/continuar/Gameover.gd
+++ scenes/continuar/Gameover.gd
@@ -4,14 +4,24 @@ extends Control
 
 
 func _on_quit_pressed():
-	Autoload.Lives = 3
-	Autoload.Charge = 6
-	get_tree().change_scene("res://scenes/menu/botones.tscn")
+	get_tree().get_nodes_in_group("sf")[0].get_node("Confirm3").play()
 	pass # Replace with function body.
 
 
 func _on_continue_pressed():
+	get_tree().get_nodes_in_group("sf")[0].get_node("Confirm4").play()
+	pass # Replace with function body.
+
+
+func _on_Confirm3_finished():
+	Autoload.Lives = 3
+	Autoload.Charge = 6
+	get_tree().change_scene("res://scenes/menu/botones.tscn")
+	pass 
+
+
+func _on_Confirm4_finished():
 	Autoload.Lives = 3
 	Autoload.Charge = 6
 	get_tree().change_scene("res://scenes/WorldTest.tscn")
-	pass # Replace with function body.
+	pass 
