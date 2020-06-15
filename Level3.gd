ac799cb5cee4562219e4a561dfb16089f28946ef
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-06-14 21:11:38 -0300

    Lvl3

--- Level3.gd
+++ Level3.gd
@@ -5,5 +5,5 @@ func _ready():
 
 
 func _on_Cdalvl3_timeout():
-	get_tree().change_scene("")
+	get_tree().change_scene("res://scenes/Level 3/Level 3.tscn")
 	pass
---
+++ scenes/Level 3/Respawn5.gd
@@ -0,0 +1,11 @@
+extends Position2D
+export (PackedScene) var ene
+func _ready():
+	pass
+
+
+func _on_TimeRes_timeout():
+	var NewEnemy = ene.instance()
+	get_tree().get_nodes_in_group("WorldTest")[0].add_child(NewEnemy)
+	NewEnemy.global_position = global_position
+	pass 
