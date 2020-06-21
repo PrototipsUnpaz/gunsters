e140db201a4e28d2f7c2fd9e6ead9f5775e6c65c
Miguel Costilla <52358916+MiguelCostilla@users.noreply.github.com>
2020-06-21 19:26:07 -0300

    Bug3

--- scenes/WorldTest.gd
+++ scenes/WorldTest.gd
@@ -2,8 +2,6 @@ extends Node2D
 var Enemy = preload("res://scenes/Enemy.tscn/")
 var Enemy_2 = preload ("res://scenes/Enemy_2.tscn/")
 func _ready():
-	Autoload.Nivel2= false
-	Autoload.Nivel3 = false
 	Autoload.canShoot = true
 	Autoload.prueba = false
 	Autoload.Lives = 3
