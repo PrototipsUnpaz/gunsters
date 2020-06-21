50fc271bb87972997ce936904446d40adb799097
Nacho <ignacioabadie@live.com.ar>
2020-06-20 22:26:42 -0300

    cooldown test

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -2,10 +2,8 @@ extends KinematicBody2D
 var Velocity = Vector2 (0,0)
 var posPistol = 0
 var Scope = preload ("res://assets/Scope.tscn")
+var canShoot = true
 
-func _ready():
-	
-	pass
 func _process(delta): 
 	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1 and Autoload.prueba == false:
 		var Sc = Scope.instance()
@@ -18,65 +16,51 @@ func _process(delta):
 	
 func _shooting():
 	$Pistolani.frame = posPistol
+	
+	if canShoot == false:    # Si la variable está en false, evita seguir ejecutando el código de shooting
+		return   # Esta return hace que termine de ejecutar este metodo, entonces ya no comprobará los "if" que siguen
+	
 	if Input.is_action_just_released("Touch") and Autoload.Charge >= 1:
 		Get_Direction()
 	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1:	
 		if get_global_mouse_position().x > 466 and get_global_mouse_position().y < 500:
-			posPistol = 1
-			Autoload.Charge -= 1
-			get_tree().call_group("Flash", "mostrar_flash")
-			
-	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1:
+			shoot(1, true)
 		if get_global_mouse_position().x < 233 and get_global_mouse_position().y < 500:
-			$Pistolani.flip_h = false
-			posPistol = 1
-			Autoload.Charge -= 1
-		get_tree().call_group("Flash", "mostrar_flash")
-	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1:
+			shoot(1, false)
 		if get_global_mouse_position().x < 233 and get_global_mouse_position().y > 500:
-			$Pistolani.flip_h = false
-			posPistol = 3
-			Autoload.Charge -= 1
-			get_tree().call_group("Flash", "mostrar_flash")
-			
-	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1:
+			shoot(3, false)
 		if get_global_mouse_position().x > 466 and get_global_mouse_position().y > 500:
-			$Pistolani.flip_h = true
-			posPistol = 3
-			Autoload.Charge -= 1
-			get_tree().call_group("Flash", "mostrar_flash")
-	if Input.is_action_just_pressed("Touch") and Autoload.Charge >= 1:
+			shoot(3, true)
 		if get_global_mouse_position().x > 234 and get_global_mouse_position().x < 465:
-			posPistol = 5
-			Autoload.Charge -= 1
-			get_tree().call_group("Flash", "mostrar_flash")
-			pass
+			shoot(5, false)
+			
 func Get_Direction():
 	if get_global_mouse_position().x > 466 and get_global_mouse_position().y < 500:
-		$Pistolani.flip_h = true
-		posPistol = 0
+		direction(1, true)
 	if get_global_mouse_position().x < 233 and get_global_mouse_position().y < 500:
-		$Pistolani.flip_h = false
-		posPistol = 0
+		direction(0, false)
 	if get_global_mouse_position().x < 233 and get_global_mouse_position().y > 500:
-		$Pistolani.flip_h = false
-		posPistol = 2
+		direction(2, false)
 	if get_global_mouse_position().x > 466 and get_global_mouse_position().y > 500:
-		$Pistolani.flip_h = true
-		posPistol = 2
+		direction(2, true)
 	if get_global_mouse_position().x > 234 and get_global_mouse_position().x < 465:
-		posPistol = 4
-	
+		direction(4, false)
 	pass
 
 
+func shoot(pos, flip):
+	canShoot = false   #  <-- Cuadno ejecuta el disparo, pone la variable en false para que no pueda disparar mas
+	$CoolDown.start()  #  <-- Cuando ejecuta el disparo, activa el timer del cooldown
+	$Pistolani.flip_h = flip
+	posPistol = pos
+	Autoload.Charge -= 1
+	get_tree().call_group("Flash", "mostrar_flash")
 
 
-
-
-
-
-
-
-
-
+func direction(pos, flip):
+	$Pistolani.flip_h = true
+	posPistol
+	
+func _on_CoolDown_timeout():
+	canShoot = true
+	pass
